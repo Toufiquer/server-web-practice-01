035fb207624bd77506a317e2b1f5f40dc478fbe4
Toufiquer <toufiquerabc@gmail.com>
2023-02-11 06:41:14 +0600

    connected server with mongo db and working

--- index.js
+++ index.js
@@ -3,9 +3,36 @@ const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
+require("dotenv").config();
+
+// Start MongoDB Server
+const uri = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.s4qhraf.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+
+    // Establish and verify connection
+    await client.db("allUsersCollection").collection("allUsers");
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+// End MongoDB Server
 
 app.get("/", (req, res) => {
   res.send({ massage: "Node server is running It is Update..." });
